28d92c5b930b3da543544269511444c75c27d5cf
rhasler1 <rhasler@luc.edu>
2025-08-28 12:33:48 -0500

    adding multiple runs to script

--- simple_benchmark/data_collection.sh
+++ simple_benchmark/data_collection.sh
@@ -1,10 +1,23 @@
-EXECUTABLE="./simple_benchmark_run"
-ITERATIONS=0
+#!/bin/bash
 
+EXECUTABLE="./simple_benchmark_run"
+OUTPUT="results.txt"
+ITERATIONS=0
+AVG_TIME=0
+
+echo "Starting simple_benchmark script, output directed to $OUTPUT"
+{
 # iterations from 10^2..10^10
 for ((i=2;i<11;i++)); do
 	ITERATIONS=$((10**i));
-	echo "Run with $ITERATIONS iterations"
-	time $EXECUTABLE $ITERATIONS
+	echo "Beginning data collection for $ITERATIONS iterations"
+	for ((j=0;j<20;j++)); do
+		echo "Run number: $j"
+		time $EXECUTABLE $ITERATIONS
+		echo
+	done
+echo "Iterations $ITERATIONS completed"
 done
+} >> $OUTPUT 2>&1
+
 echo "Done"
