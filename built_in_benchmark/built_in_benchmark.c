30c10fe7ba5c6b800fe5a8db58c8bf7a62144afa
rhasler1 <rhasler@luc.edu>
2025-08-28 15:08:47 -0500

    adding code for section 3

---
+++ built_in_benchmark/built_in_benchmark.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char** argv) {
+    if (argc != 2) return 1;
+    int n = atoi(argv[1]);
+
+    struct timespec tstart={0,0}, tend={0,0};
+    clock_gettime(CLOCK_MONOTONIC, &tstart);
+    
+    for(int k = 0; k < n; k++) {
+        asm("nop");
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &tend);
+    
+    printf("loop took about %.5f seconds\n",
+    ((double)tend.tv_sec + 1.0e-9*tend.tv_nsec) -
+    ((double)tstart.tv_sec + 1.0e-9*tstart.tv_nsec));
+    return 0;
+}
---
+++ built_in_benchmark/data_collection.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+EXECUTABLE="./built_in_benchmark_run"
+OUTPUT="results.txt"
+ITERATIONS=0
+AVG_TIME=0
+
+echo "Starting $EXECUTABLE, output directed to $OUTPUT"
+{
+# iterations from 10^2..10^10
+for ((i=2;i<11;i++)); do
+	ITERATIONS=$((10**i));
+	echo "Beginning data collection for $ITERATIONS iterations"
+	for ((j=0;j<20;j++)); do
+		echo "Run number: $j"
+		$EXECUTABLE $ITERATIONS
+		echo
+	done
+echo "Iterations $ITERATIONS completed"
+done
+} >> $OUTPUT 2>&1
+
+echo "Done"
